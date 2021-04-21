a22ddbf52b8390b8c5fa53b0650ab34a9fb79616
Javi Romero <elmurci@gmail.com>
2021-04-21 12:26:22 +0200

    feat(0.0.2): version

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -4,9 +4,10 @@
     <div class="container mx-auto px-8">
       <div class="w-full flex flex-col md:flex-row py-6">
         <div class="flex-1 mb-6 text-black">
-          <a class="text-pink-600 no-underline hover:no-underline text-xl lg:text-2xl" href="#">
-            <img src="/img/xrp-symbol-black.svg" class="float-left" width="45" />
+          <a class="text-pink-600 no-underline hover:no-underline text-xl lg:text-2xl" href="/">
+            <img src="/img/xrp-symbol-black.svg" width="45" />
           </a>
+          <div class="text-gray-200 ml-1 mt-8">v{{ version }}</div>
         </div>
         <div class="flex-1">
           <p class="uppercase md:mb-6 text-green-600">Links</p>
@@ -75,6 +76,7 @@ import { Component, Vue } from 'vue-property-decorator'
   }
 })
 export default class AppFooter extends Vue {
+  version = require('../../../lib/version')
   created () {
     //
   }
