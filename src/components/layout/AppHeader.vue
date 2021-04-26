911fe4b1f78df7334ce1e89df03a9573a634f167
Elliot Lee <github.public@intelliot.com>
2021-04-26 16:42:20 -0700

    feat: add homepage content

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -71,7 +71,7 @@
           <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Apply
+            Apply Now
           </button>
         </div>
       </div>
