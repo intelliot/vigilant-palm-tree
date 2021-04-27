00871af07332c6b78de0b7445722aaadf62bbcb3
Elliot Lee <github.public@intelliot.com>
2021-04-26 23:05:26 -0700

    fix: speed up FAQ transitions and fill in placeholder

--- src/pages/FAQ.vue
+++ src/pages/FAQ.vue
@@ -237,10 +237,10 @@
           <div class="w-full sm:w-1/2 p-6 mt-6">
             <div class="align-middle">
               <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
-                Lorem ipsum dolor sit amet
+                XRPL App Grants
               </h3>
               <p class="text-gray-600 mb-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                Supporting the open source ecosystem around the XRP Ledger in order to boost inclusive standards and innovation in the Internet of Value.
               </p>
             </div>
           </div>
